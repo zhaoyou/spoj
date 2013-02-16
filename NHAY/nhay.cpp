240dfe40056eb291cc8fd946f6bce6c328c9e637
zhaoyou <zhaoyou.xt@gmail.com>
2013-02-16 15:29:22 +0800

    finsh nhay , use printf instead of cout

--- NHAY/nhay.cpp
+++ NHAY/nhay.cpp
@@ -3,29 +3,6 @@
 
 using namespace std;
 
-void getOverlay(string pattern, int *p) {
-
-  int m = pattern.size();
-
-  p[0] = -1;
-
-  for (int i = 0; i < m ;i++) {
-    int index = -1;
-
-    string sub = pattern.substr(0, i+1);
-
-    int sub_size  = sub.size();
-    for(int j = 1; j < sub_size; j++) {
-      if (sub.substr(0, sub_size - j ) == sub.substr(j , sub_size)) {
-        index = sub_size - j -1;
-        break;
-      }
-    }
-    p[i] = index;
-    //cout << "substring: " << sub << " index: " << index << endl;
-  }
-}
-
 void getOverlay2(string pattern, int *overlay_function) {
     const int pattern_length = pattern.size();
     int index;
@@ -48,7 +25,7 @@ void getOverlay2(string pattern, int *overlay_function) {
         }
     }
     //for(int i = 0; i < pattern_length; i++) {
-    //  cout << overlay_function[i] << endl;
+    //  printf("%d\n", overlay_function[i]);
     //}
 }
 
@@ -62,24 +39,22 @@ void calc(char *pattern, int *p, int pattern_len) {
 
   while(c != '\n') {
 
-    //cout << "********" << endl;
     while(c != pattern[pattern_index] && pattern_index > 0 ) {
       pattern_index = p[pattern_index - 1] + 1;
-     // cout << c << ": " << pattern_index << endl;
     }
     if (c == pattern[pattern_index]) {
       pattern_index++;
     }
 
     if (pattern_index == pattern_len) {
-      cout << "index: " << pattern_index << ":" << index - pattern_index << endl;
+      printf("%d\n", index -pattern_index);
       pattern_index = p[pattern_index-1] + 1;
     }
 
     index++;
     scanf("%c", &c);
   }
-  cout << endl;
+  printf("\n");
 }
 
 
